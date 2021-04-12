ed8f070aa93db3b231fd819c7e5924a4200033bc
Rémi Vanicat <vanicat@debian.org>
2021-04-12 11:29:52 +0200

    Refactoring
    Lets move function and class in their specific file.

---
+++ old_interface.py
@@ -0,0 +1,278 @@
+from collections import namedtuple
+from typing import OrderedDict
+import networkx as nx
+from satisfactory_model import db
+from IPython.display import display, Markdown, clear_output
+import math
+import ipywidgets as widgets
+
+ProducedItem = namedtuple('ProducedItem', ['quantity', 'recipe'])
+
+class Option:
+    """Option(name, time, amount, ingredients, subproduct) : time is in seconde!"""
+    def __init__(self, name, time, amount, ingredients, subproduct):
+        self.name = name
+        self.time = time / 60
+        self.amount = amount
+        self.ingredients = ingredients
+        self.subproduct = subproduct
+        
+    def __iter__(self):
+        return iter(self.ingredients)
+        
+    def __str__(self):
+        result = f"{self.name} {self.amount / self.time}"
+        if self.ingredients:
+            result += ':'
+        for name, amount in self.ingredients:
+            result += f' {name}: {amount / self.time}'
+        return result
+    
+    def __repr__(self):
+        return f"Option({self.name}, {self.time}, {self.amount}, {self.ingredients})"
+    
+    def speed(self):
+        return self.amount / self.time
+        
+    def tick_per_m(self, amount):
+        return amount / self.amount
+    
+    def usines(self, amount):
+        usine_tick_per_m = 1 / self.time
+        usines = self.tick_per_m(amount) / usine_tick_per_m
+        return usines
+    
+    def produce(self, amount):
+        ingredient = [(product, q * self.tick_per_m(amount)) for (product, q) in self.ingredients]
+        subproduct = [(product, q * self.tick_per_m(amount)) for (product, q) in self.subproduct]
+        return ingredient, subproduct
+
+class ResultFromProd:
+    """Result(product, quantity) make an object to search how to build stuff.
+    
+    quantity is a quantity by minute"""
+    def __init__(self, product, quantity):
+        self.produced =  OrderedDict()
+        self.needed = {}
+        self.subproduct = {}
+        self.options = {}
+        self.depend = nx.DiGraph()
+        self.depend.add_node(product)
+        self.quantity = quantity
+        self.add_needed(product, 1)
+        
+        
+    def set_quantity(self, q):
+        self.quantity = q
+        
+    
+    def add_needed(self, product, quantity):
+        if product in self.subproduct:
+            quantity = quantity - self.subproduct[product]
+            if quantity < 0:
+                self.subproduct[product] =  - quantity
+                return
+            elif quantity == 0:
+                del self.subproduct[product]
+                return
+        if product in self.needed:
+            self.needed[product] += quantity
+        elif product in self.produced:
+            self.add_produced(product, quantity)
+        else:
+            self.needed[product] = quantity
+            possibility = db.search_possibility_by_product(product)
+            
+            new_options = {}
+            for r_id, r_name, r_time, r_amount in possibility:
+                ingredients = db.get_ingredients(r_id)
+                subproducts = db.get_subproducts(r_id)
+                new_options[r_name] = Option(r_name, r_time, r_amount, ingredients, subproducts)
+            self.options[product] = new_options
+
+    def show_options(self):
+        return self.options
+
+    def show_produced(self):
+        result = {}
+        for product in nx.topological_sort(self.depend):
+            if product in self.produced:
+                option = self.produced[product]
+                q = option.quantity * self.quantity
+                result[product] = (q, option.recipe and option.recipe.usines(q))
+        return result
+
+    def show_subproduct(self):
+        return {p: q * self.quantity for (p, q) in self.subproduct.items()}
+
+    def auto_option(self, product):
+        return product in self.options[product] or len(self.options[product]) <= 1
+
+        
+    def choose_option(self, product, option = None):
+        if product in self.produced:
+            print(f'{product} already there')
+            self.accept_produced(product)
+            return
+
+        if len(self.options[product]) > 0:
+            if option is None and product not in self.options[product]:
+                if len(self.options[product]) != 1:
+                    raise ValueError(f"option cannot be None for {product}")
+                _, recipe = self.options[product].popitem()
+            else:
+                if option is None:
+                    option = product
+                recipe = self.options[product][option]
+                
+            needed, subproduct = recipe.produce(self.needed[product])
+                
+            for i_name, i_q in needed:                
+                self.add_needed(i_name, i_q)
+                self.depend.add_node(i_name)
+                self.depend.add_edge(i_name, product)
+                
+            for i_name, i_q in subproduct:
+                if i_name != product:
+                    if i_name in self.subproduct:
+                        self.subproduct[i_name] += i_q
+                    else:
+                        self.subproduct[i_name] = i_q
+            
+            self.accept_produced(product, recipe)
+        else:
+            self.accept_produced(product)
+    
+    def accept_produced(self, product, recipe = None):
+        assert product not in self.produced
+
+        self.produced[product] = ProducedItem(quantity = self.needed[product], recipe = recipe)
+        
+        del self.options[product]
+        del self.needed[product]
+                
+    def add_produced(self, product, quantity):
+        assert product in self.produced
+        old = self.produced[product]
+        self.produced[product] = ProducedItem(quantity = old.quantity + quantity, recipe = old.recipe)
+        
+        if old.recipe: # TODO !!! tenir compte des sous produits en plus.
+            for i_name, i_q in old.recipe.produce(quantity)[0]:
+                self.add_needed(i_name, i_q)
+        
+        
+    def auto(self):
+        for product in self.needed:
+            if self.auto_option(product):
+                self.choose_option(product)
+
+class DisplayProd:
+    def __init__(self, product, quantity):
+        assert False, 'going to second!'
+        self.product = product
+        self.quantity = quantity
+        
+        self.model = ResultFromProd(product, quantity)
+        self.title = widgets.Label(f"Objectif: {product}: ")
+        self.quantity_field = widgets.Text()
+        self.quantity_field.value = str(quantity)
+        self.quantity_field.on_submit(self.on_set_value)
+        
+        self.producing = widgets.Output()
+        self.options = widgets.VBox()
+        self.auto = widgets.Button(description='Auto')
+        self.restart = widgets.Button(description='Restart')
+        
+        self.auto.on_click(self.on_auto)
+        self.restart.on_click(self.on_restart)
+        
+        self.box = widgets.VBox([widgets.HBox((self.title, self.quantity_field)), self.producing, self.options, widgets.HBox((self.auto, self.restart)) ])
+        
+        self.update()
+        
+    def update(self):
+        self.update_producing()
+        self.update_options()
+        
+    def update_producing(self):
+        with self.producing:
+            clear_output()
+            self.print_producing()
+            self.print_subproduct()
+            
+    def print_producing(self):
+        if self.model.show_produced():
+            print(f"to produce:")
+            for product, (q, u) in self.model.show_produced().items():
+                if u is None:
+                    print(f"  {product}: {q}")
+                else:
+                    print(f"  {product}: {q} dans {math.ceil(u)} usine(s) à {u/math.ceil(u)*100}%")
+        else:
+            print("to produce: nothing yet")
+            
+    def print_subproduct(self):
+        if self.model.show_subproduct():
+            print("\n\nResidue:")
+            for (product, q) in self.model.show_subproduct().items():
+                print(f"  {product}: {q}")
+        
+    def update_options(self):
+        opt_widgets = []
+        title = widgets.Label('options:')
+        opt_widgets.append(title)
+        
+        for product, recipe_list in self.model.show_options().items():
+            label = widgets.Label(product + ": ")
+            
+            choose = [("", None)]
+            for recipe, ingredient in recipe_list.items():
+
+                choose.append((str(ingredient), recipe))
+
+            radio = widgets.RadioButtons(options = choose, layout = widgets.Layout(width='70%'))
+        
+            button_c = widgets.Button(description = "choose")
+            button_c.on_click(self.on_choose(product, radio))
+        
+            button_p = widgets.Button(description = "produced")
+            button_p.on_click(self.on_produced(product))
+        
+            box = widgets.HBox((label, radio, widgets.VBox((button_c, button_p))))
+            
+            opt_widgets.append(box)
+        
+        opt_widgets.reverse()
+        self.options.children = tuple(opt_widgets)
+        
+    def on_set_value(self, _):
+        self.model.set_quantity(float(self.quantity_field.value))
+        self.quantity = float(self.quantity_field.value)
+        self.update()
+        
+        
+    def on_auto(self, _):
+        self.model.auto()
+        self.update()
+        
+    def on_restart(self, _):
+        self.model = ResultFromProd(self.product, self.quantity)
+        self.update()
+        
+    def on_choose(self, product, radio):
+        def doit(_):
+            self.model.choose_option(product, radio.value)
+            print(f"choosing : {product} with {radio.value}: choose_option({repr(product)}, {repr(radio.value)})")
+            self.update()
+        return doit
+    
+    def on_produced(self, product):
+        def doit(_):
+            self.model.accept_produced(product)
+            print(f"{product} accedeted")
+            self.update()
+        return doit
+
+    @property
+    def display(self):
+        return self.box
---
+++ satisfactory_model.py
@@ -0,0 +1,228 @@
+from collections import OrderedDict, namedtuple
+from interactive_satisfactory import interactiveOfProduction
+from utils import myround
+from satisfactory_db import SatisfactoryDb
+from IPython.display import display
+import math
+
+db = SatisfactoryDb()
+
+Recipe = namedtuple('Recipe', ['name', 'alternate', 'time', 'ingredient', 'product', 'producedIn'])
+
+builder = {
+    'Desc_AssemblerMk1_C': 'Assembler',
+    'Desc_ConstructorMk1_C': 'Constructor',
+    'Desc_FoundryMk1_C': 'Foundry',
+    'Desc_ManufacturerMk1_C': 'Manufacturer',
+    'Desc_OilRefinery_C': 'Refinery',
+    'Desc_SmelterMk1_C': 'Smelter',
+    'Desc_Packager_C': 'Packager',
+    'Desc_Blender_C': 'Blender',
+    'Desc_HadronCollider_C': 'Hadron Collider',
+    None: None
+}
+
+
+# In[18]:
+all_recipes = {}
+
+for (r_id, _, name, _, alternate, time, _, _, _, _, _, producedIn) in db.list_all_recipes:
+    ingredients = db.get_ingredients(r_id)
+    products = db.get_subproducts(r_id)
+
+    if producedIn in builder:
+        producedIn = builder[producedIn]
+    
+    if name in all_recipes and all_recipes[name] != Recipe(name, alternate, time, ingredients, products, producedIn):
+        print(f"{name} déjà là: {all_recipes[name]} et {(name, alternate, time, ingredients, products, producedIn)} ")
+    all_recipes[name] = Recipe(name, alternate, time / 60, ingredients, products, producedIn)
+
+current_result = None
+
+class ResultOfProd:
+    """ResultOfProd() make an object where you can add ressource, or recipe to plan production line
+    
+    quantity is a quantity by minute"""
+    def __init__(self, name = "None", sort = True):
+        self.available =  {}
+        self.needed =  {}
+        self._recipes = {}
+        self.constructed = {}
+        self.name = name
+        self.sort = sort
+        
+    def construct(self, recipe, q):
+        if recipe in self.constructed:
+            self.constructed[recipe] += q
+        else:
+            self.constructed[recipe] = q
+        
+    def add_product(self, p, q):
+        if p in self.available:
+            self.available[p] += q
+        else:
+            self.available[p] = q
+    
+    def consume_product(self, p, q):
+        """Warning: you can consume product that are not there"""
+        if p in self.needed:
+            self.needed[p] += q
+        else:
+            self.needed[p] = q
+        
+    def add_node(self, p, quality):
+        q = 300/1
+        if quality == "pure":
+            q = 780/1
+        elif quality == "normal":
+            q = 600/1
+        self.add_product(p, q)
+        
+    def add_recipe(self, name, n):
+        """add n producter using recipe"""
+        assert name in all_recipes
+        if name in self._recipes:
+            self._recipes[name] += n
+        else:
+            self._recipes[name] = n
+            
+        for item, quantity in all_recipes[name].ingredient:
+            self.consume_product(item, quantity * n / all_recipes[name].time)
+            
+        for item, quantity in all_recipes[name].product:
+            self.add_product(item, quantity * n / all_recipes[name].time)
+            
+    def consume_with_recipe(self, recipe_name, product, q = None):
+        if getattr(q, '__getitem__', False):
+            q = q[product]
+        if q is None:
+            q = self[product]
+        recipe = all_recipes[recipe_name]
+        n = 0
+        for p, q2 in recipe.ingredient:
+            if p == product:
+                n = q / (q2 / recipe.time)
+        
+        self.add_recipe(recipe_name, n)
+    
+    def produce_with_recipe(self, recipe_name, product, q = None):
+        if getattr(q, '__getitem__', False):
+            q = -q[product]
+        if q is None:
+            q = -self[product]
+        recipe = all_recipes[recipe_name]
+        n = 0
+        for p, q2 in recipe.product:
+            if p == product:
+                n = q / (q2 / recipe.time)
+        
+        self.add_recipe(recipe_name, n)
+        
+    
+    def strs_products(self):
+        products = set(self.available.keys()).union(set(self.needed.keys()))
+        result = []
+        for p in products:
+            if p in self.available and p in self.needed:
+                qa = myround(self.available[p])
+                qn = myround(self.needed[p])
+                result.append((qa-qn, f"{qa-qn} {p}: {qn} of {qa}"))
+            elif p in self.available:
+                qa = myround(self.available[p])
+                result.append((qa, f"{qa} {p}: +{qa}"))
+            else:
+                qn = myround(self.needed[p])
+                result.append((-qn, f"{-qn} {p}: -{qn}"))
+                
+        if self.sort:
+            result.sort()
+        
+        return [ st for _, st in result ]
+    
+    def products(self):
+        products = set(self.available.keys()).union(set(self.needed.keys()))
+        result = []
+        for p in products:
+            if p in self.available and p in self.needed:
+                qa = myround(self.available[p])
+                qn = myround(self.needed[p])
+                result.append((qa-qn, f"{qa-qn} {p}: {qn} of {qa}", p))
+            elif p in self.available:
+                qa = myround(self.available[p])
+                result.append((qa, f"{qa} {p}: +{qa}", p))
+            else:
+                qn = myround(self.needed[p])
+                result.append((-qn, f"{-qn} {p}: -{qn}", p))
+                
+        if self.sort:
+            result.sort()
+        
+        return (( (st, item, q) for q, st, item in result ))
+    
+    def strs_recipes(self):
+        result = []
+        for recipe, q in self._recipes.items():
+            if q != 0:
+                if recipe in self.constructed:
+                    sts = f"{myround(self.constructed[recipe])} of {myround(q)} {all_recipes[recipe].producedIn} using {recipe}, reste {myround(q - self.constructed[recipe])}"
+                    if self.constructed[recipe] >= q:
+                        sts = '# ' + sts
+                    result.append(sts)
+                else:
+                    result.append(f"{myround(q)} {all_recipes[recipe].producedIn} using {recipe}")
+        return result
+    
+    def recipes(self):
+        for recipe, q in self._recipes.items():
+            if q != 0:
+                if recipe in self.constructed:
+                    sts = f"{self.constructed[recipe]} of {myround(q)} {all_recipes[recipe].producedIn} using {recipe}, reste {myround(q - self.constructed[recipe])}"
+                    if self.constructed[recipe] >= q:
+                        sts = '# ' + sts
+                    yield sts, recipe, q - self.constructed[recipe]
+                else:
+                    yield f"{myround(q)} {all_recipes[recipe].producedIn} using {recipe}", recipe, q
+        return None
+
+    def building(self):
+        result = {}
+        for recipe, q in self._recipes.items():
+            if q != 0:
+                p = all_recipes[recipe].producedIn
+                c = self.constructed.get(recipe, 0)
+                q = math.ceil(q)
+                if p in result:
+                    old_c, old_q = result[p]
+                    result[p] = (old_c + c, old_q + q)
+                else:
+                    result[p] = (c, q)
+        return result
+    
+    def __enter__(self):
+        global current_result
+        current_result = self
+        return self
+    
+    def __exit__(self, *args):
+        if len(self.name) < 4 or self.name[0:4] != '    ':
+            name = '    ' + self.name
+        else:
+            name = self.name
+        display(interactiveOfProduction(self, name, db))
+    
+    def __getitem__(self, name):
+        qa = self.available[name] if name in self.available else 0
+        qn = self.needed[name] if name in self.needed else 0
+        return qa - qn
+
+# In[20]:
+for method in vars(ResultOfProd):
+    if len(method) > 2 and method[0:2] == '__':
+        continue
+    def f(m):
+        return lambda *args: getattr(current_result, m)(*args)
+    globals()[method] = f(method)
+
+def items(it):
+    return current_result[it]
+
