55488747bc70c7dc57e38527d02e15af456dc4e9
Rémi Vanicat <vanicat@debian.org>
2021-04-13 21:38:43 +0200

    add current_result to satifactory

--- satifactory.py
+++ satifactory.py
@@ -1,7 +1,7 @@
 from satisfactory_model import (db, ResultOfProd, 
     construct, add_product, add_node, add_recipe, 
     consume_with_recipe, produce_with_recipe,
-    products, recipes, building, items)
+    products, recipes, building, items, current_result)
 
 
 def shopping_list(buildings_dict):
