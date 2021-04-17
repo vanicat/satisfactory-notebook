3fc2415bc1aa747c62c8da2c930d88826979d584
Rémi Vanicat <vanicat@debian.org>
2021-04-17 11:48:26 +0200

    Make export more explicit

--- satisfactory/__init__.py
+++ satisfactory/__init__.py
@@ -1,7 +1,14 @@
 from .db import db
-from .model import Model
+from .model import Model, Production
 from .ui import interactiveOfProduction
 from IPython.display import display
+
+__all__ = [
+    'db', 'Model', 'interactiveOfProduction', 'ResultOfProd', 'shopping_list',
+    'Production', 'construct', 'add_product', 'consume_product', 'add_recipe',
+    'consume_with_recipe', 'produce_with_recipe', 'products',
+    'recipes', 'building', 'items'
+]
 
 def shopping_list(buildings_dict):
     return db.shopping_list(buildings_dict)
@@ -17,19 +24,22 @@ class ResultOfProd(Model):
         return self
     
     def __exit__(self, *args):
-        name = self.name
-        if self.name and (len(self.name) < 4 or self.name[0:4] != '    '):
-            name = '    ' + self.name
-        else:
-            name = self.name
+        if self.name is not None:
+            name = self.name.strip()
         display(interactiveOfProduction(self, name, db, self.margin))
 
-for method in vars(Model):
-    if len(method) > 2 and method[0:2] == '__':
-        continue
-    def f(m):
-        return lambda *args, **kwargs: getattr(current_result, m)(*args, **kwargs)
-    globals()[method] = f(method)
+def make_function_from_method(m):
+    return lambda *args, **kwargs: getattr(current_result, m)(*args, **kwargs)
+
+construct = make_function_from_method('construct')
+add_product = make_function_from_method('add_product')
+consume_product = make_function_from_method('consume_product')
+add_recipe = make_function_from_method('add_recipe')
+consume_with_recipe = make_function_from_method('consume_with_recipe')
+produce_with_recipe = make_function_from_method('produce_with_recipe')
+products = make_function_from_method('products')
+recipes = make_function_from_method('recipes')
+building = make_function_from_method('building')
 
 def items(it):
     return current_result[it]
